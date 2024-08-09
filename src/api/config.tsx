03685b6aa9e4a70c706a45afde9b242fe5453d5f
sgullen-dev <96399361+sgullen-dev@users.noreply.github.com>
2024-08-09 17:38:17 -0400

    Update config.tsx

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,9 +1,6 @@
 // MARTA real time arrivals tracker API
 export const martaArrivalsURL: string =
-  'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals';
+  'https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/traindata';
 
 // API key for MARTA web service
 export const apiKey: string = '6ebc6071-dc42-4eb2-b22d-37ccb8de6662';
-
-// cors-anywhere proxy
-export const corsProxyURL: string = 'https://polar-brook-24053.herokuapp.com/';
